718d2320bd7574973ed8c06db20b9d46a5b48405
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-15 21:02:00 +0100

    The Math Library

---
+++ src/main/kotlin/Math.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // rounds decimal number up to the next whole number
+    println(Math.ceil(6.9))
+
+    // rounds the number down to previous whole number
+    println(Math.floor(6.9))
+
+    // takes the absolute value of number
+    println(Math.abs(-30))
+
+    // square-root of number
+    println(Math.sqrt(25.0))
+}
