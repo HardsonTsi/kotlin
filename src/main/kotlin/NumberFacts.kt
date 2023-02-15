e8fa6fcb8744530122d14946c22d988ec23abe12
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-15 19:24:50 +0100

    operations, argumented assignments and ncrement and decrements operators

---
+++ src/main/kotlin/NumberFacts.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var speedOfLight = 186_000
+
+    // argumented assignments operators
+
+    // speedOfLight = speedOfLight + 282
+    speedOfLight += 282
+
+    // speedOfLight = speedOfLight - 282
+    speedOfLight -= speedOfLight
+
+    // speedOfLight = speedOfLight * 282
+    speedOfLight *= 282
+
+    // speedOfLight = speedOfLight / 282
+    speedOfLight /= 282
+
+    // speedOfLight = speedOfLight % 3
+    speedOfLight %= 3
+
+    // increment and decrements operators
+
+    var apples = 5
+    apples++
+    println(apples)
+
+
+}
