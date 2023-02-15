05dcbcc1292bfde8eefacc219d69b8d5bb0d7387
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-15 18:41:01 +0100

    String properties and functions

---
+++ src/main/kotlin/StringPropertiesAndFunctions.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val word = "sfjpowiefhoiwehfoigwhgiohfoa"
+
+    //word length
+    println(word.length)
+
+    //word to capitalize
+    println(word.capitalize())
+
+    //word ro uppercase
+    println(word.uppercase())
+
+    // ...
+
+
+
+}
