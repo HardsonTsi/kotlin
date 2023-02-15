aacd6731189256dc13e65f9fca2304f29c1dc632
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-15 18:05:16 +0100

    mutable and immutable variable

---
+++ src/main/kotlin/Date.kt
@@ -0,0 +1,9 @@
+fun main(){
+    // mutable variable
+    var todaysDate: String = "02/15/2002"
+    println(todaysDate)
+
+    // immutable variable
+    val goldenRatio: Double = 1.618
+    val pi: Double = 3.14
+}
