ec117214653f2bc7fe847e1e73bc040175776aac
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-17 13:15:40 +0100

    Labeled Jump Expressions

--- src/main/kotlin/ColumnsRows.kt
+++ src/main/kotlin/ColumnsRows.kt
@@ -2,36 +2,25 @@
                                     Jump Expressions
        The jump expressions, break and continue, are used to change the standard
        loop behavior by exiting a loop early or skipping a single repetition
+
+                                    Labeled Jump Expressions
+        "How would use jump expressions inside nested loop ?"
+        Good question ! The answer is labeled jump expressions. By adding a label to outer loop
+        of nested loops, we can execute a jump expression from the inner loop and have it act
+        on the labeled outer loop
 */
 
 fun main() {
 
-    for (i in 1..6) {
+    grid@ for (i in 1..6) {
         for (j in 'A'..'F') {
+            if (j == 'C') {
+                continue@grid
+            }
             print("$j$i ")
         }
         println()
     }
 
-    val rooms = listOf("Kitchen", "Living Room", "Basement", "Bathroom")
-    //search Living Room
-    println("---Room Search---")
-    for (room in rooms) {
-        print("$room: ")
-        if (room == "Living Room") {
-            println("Found it !")
-            break
-        }
-        println("Found nothing.")
-    }
-
-    // skip a loop iteration
-    println("---Divide by Zero---")
-    for (number in 12 downTo -12 step 4) {
-        if (number == 0) {
-            continue
-        }
-        println("120/$number = ${120 / number}")
-    }
 
 }
