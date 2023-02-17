6196134b00e3b8948877101822b877dd7456d048
TESSI Hardson <hardsontessi2@gmail.com>
2023-02-17 14:49:29 +0100

    Return Statements

--- src/main/kotlin/Recipe.kt
+++ src/main/kotlin/Recipe.kt
@@ -1,5 +1,17 @@
+/*
+   A return statement returns a value from a function and passes it back to where the function
+   was invocked. This value can then be used throughout out program
+*/
 
+
+
+fun ozToTsp(oz: Double): Double {
+    var tsp = oz * 6
+    return tsp
+}
 
 fun main() {
-
+    // invoke ozToTsp() function
+    var tspNeedded = ozToTsp(.75)
+    println("You will need $tspNeedded tsp of vanilla extract for this recipe.")
 }
